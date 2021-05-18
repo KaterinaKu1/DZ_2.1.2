7e5862c8ab7b4b97b2f4369b130d93f7847481fa
KaterinaKu1 <katya@sptour.by>
2021-05-18 15:19:00 +0300

    addingFile

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        double initialAccount = 100;
-        double refill = 1000;
+        double initialAccount = 100.1;
+        double refill = 1000.5;
         boolean bonus = refill > 1000;
 
         if (bonus) {
             int balance = (int) (refill / 100);
-            System.out.println("Итоговая сумма " + (initialAccount + balance + refill));
+            System.out.println("Итоговая сумма " + (int) (initialAccount + balance + refill));
 
         } else {
-            System.out.println("Итоговая сумма " + (initialAccount + refill));
+            System.out.println("Итоговая сумма " + (int) (initialAccount + refill));
         }
     }
 }
